075f63ec341de1516107bf060e05e74e74345e49
ambrt <ambrth@gmail.com>
2021-02-03 16:09:00 +0100

    different default

--- src/fence.js
+++ src/fence.js
@@ -5,8 +5,13 @@ module.exports = {
 		return {
 			plugin: function (markdownIt, _options) {
                 
-
+                
                 const defaultRender = markdownIt.renderer.rules.fence.bind(markdownIt.renderer.rules)
+                //This also doesn't work
+                
+                //const defaultRender = markdownIt.renderer.rules.fence || function(tokens, idx, options, env, self) {
+				//	return self.renderToken(tokens, idx, options, env, self);
+				//};
                   
             
                 markdownIt.renderer.rules.fence = function(tokens, idx, options, env, self) {
